ee7c34139ac89d017c2d2a25e8261782051093bf
apdo <apdo>
2025-06-11 09:01:17 +0300

    OAuth2

--- config/oauth.config.js
+++ config/oauth.config.js
@@ -4,7 +4,7 @@ module.exports = {
   google: {
     clientID: process.env.GOOGLE_CLIENT_ID,
     clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-    callbackURL: `https://apdo978.github.io/Abdelrhman/api/v1/auth/google/callback`
+    callbackURL: `https://e-commerce-mean-production.up.railway.app/api/v1/auth/google/callback`
   },
   session: {
     secret: process.env.SESSION_SECRET,
